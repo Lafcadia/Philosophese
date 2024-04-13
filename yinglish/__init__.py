90b07c3b169ca1b39e598502c9c668c31bd7fb9b
Zaixu Zheng <147896059+Lafcadia@users.noreply.github.com>
2024-04-13 17:18:56 +0800

    Update __init__.py

--- yinglish/__init__.py
+++ yinglish/__init__.py
@@ -5,13 +5,13 @@ import jieba.posseg as pseg
 jieba.setLogLevel(20)
 
 
-def _词转换(x, y, 淫乱度):
-    if random.random() > 淫乱度:
+def _convertion(x, y, philo):
+    if random.random() > philo:
         return x
     if x in {'，', '。'}:
-        return '……'
+        return '♂'
     if x in {'!', '！'}:
-        return '❤'
+        return '♀'
     if len(x) > 1 and random.random() < 0.5:
         return f'{x[0]}……{x}'
     else:
@@ -20,9 +20,9 @@ def _词转换(x, y, 淫乱度):
         return f'……{x}'
 
 
-def chs2yin(s, 淫乱度=0.5):
-    return ''.join([_词转换(x, y, 淫乱度) for x, y in pseg.cut(s)])
+def chs2yin(s, philo=0.5):
+    return ''.join([_convertion(x, y, philo) for x, y in pseg.cut(s)])
 
 
 if __name__ == '__main__':
-    print(chs2yin('不行，那里不行。'))
+    print(chs2yin('从前的哲学家们只是用不同的方式解释世界，而问题在于改变世界。'))
