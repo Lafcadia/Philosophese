44a2e7bea5ad335f44aec5c135ebef22e1b4379b
Zaixu Zheng <147896059+Lafcadia@users.noreply.github.com>
2024-04-13 17:24:48 +0800

    Update __init__.py

--- yinglish/__init__.py
+++ yinglish/__init__.py
@@ -14,15 +14,15 @@ def _convertion(x, y, philo):
         return '♀'
     if len(x) > 1 and random.random() < 0.5:
         return f'{x[0]}……{x}'
-    else:
-        if y == 'n' and random.random() < 0.5:
-            x = '〇' * len(x)
-        return f'……{x}'
+    # else:
+    #     if y == 'n' and random.random() < 0.5:
+    #         x = '〇' * len(x)
+    #     return f'……{x}'
 
 
-def chs2yin(s, philo=0.5):
+def chs2phi(s, philo=0.5):
     return ''.join([_convertion(x, y, philo) for x, y in pseg.cut(s)])
 
 
 if __name__ == '__main__':
-    print(chs2yin('从前的哲学家们只是用不同的方式解释世界，而问题在于改变世界。'))
+    print(chs2phi('从前的哲学家们只是用不同的方式解释世界，而问题在于改变世界。'))
